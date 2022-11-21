e64d7f4489a470fe401eb4800c450e003888a5a8
Linden <wheelerlinden@gmail.com>
2022-11-21 17:51:23 -0500

    constructors + get/set

---
+++ Article.java
@@ -0,0 +1,123 @@
+public class Article {
+    //this will have all the getters and setters + toStrings for all articles
+    
+    private String author;
+    private String journal;
+    private String title;
+    private String year;
+    private String volume;
+    private String number;
+    private String pages;
+    private String keywords;
+    private String doi;
+    private String ISSN;
+    private String month;
+    /**
+	 * Constructor takes in parameters and creates a new Article object
+	 * @param author name of author of the article.
+	 * @param journal type of journal(IEEE, ACM, NJ)
+	 * @param title name of the article
+	 * @param year year article was published
+	 * @param volume volume of article.
+	 * @param number number of article.
+	 * @param pages pages of article.
+	 * @param keywords keywords of article.
+	 * @param doi doi of article.
+	 * @param ISSN ISSN of article.
+	 * @param month month article was published.
+	 */
+    public Article(String author, String journal, String title, String year, String volume, String number, String pages, String keywords, String doi, String ISSN, String month)
+    {
+        this.author = author;
+        this.journal = journal;
+        this.title = title;
+        this.year = year;
+        this.volume = volume;
+        this.number = number;
+        this.pages = pages;
+        this.keywords = keywords;
+        this.doi = doi;
+        this.ISSN = ISSN;
+        this.month = month;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getDoi() {
+        return doi;
+    }
+    public void setDoi(String doi) {
+        this.doi = doi;
+    }
+
+    public String getISSN() {
+        return ISSN;
+    }
+    public void setISSN(String iSSN) {
+        ISSN = iSSN;
+    }
+    
+    public String getMonth() {
+        return month;
+    }
+    public void setMonth(String month) {
+        this.month = month;
+    }
+    
+
+
+}
