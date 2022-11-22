a424d56e09d42cfe1fb1fb40bbb5fd838625ad58
Linden <wheelerlinden@gmail.com>
2022-11-22 12:50:55 -0500

    added comments

--- BibliographyFactory.java
+++ BibliographyFactory.java
@@ -5,6 +5,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.io.File;
+//driver class
+//im thinking we make a loop that goes through every file and puts each element into an array. then writes them into the files? idk fam
+//look up use delimeter it miht be useful for this not sure
 public class BibliographyFactory {
     
 }
--- FileInvalidException.java
+++ FileInvalidException.java
@@ -1,3 +1,4 @@
 public class FileInvalidException extends Exception {
-    
+    //we need file not found exception and file invalid for when one of the descriptions is blank
+    //im avoiding this cause i barely know exceptions but it shouldn't be that hard
 }
